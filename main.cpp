d2e45c4f44d243bc2caca71072c36420de2abc0f
xmh0511 <970252187@qq.com>
2019-05-23 17:26:19 +0800

    update

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace xfinal;
 int main(std::size_t argus_size, char const* arugs[])
 {
 	std::string port = "8080";
-	std::string root_path = "D:\\";
+	std::string root_path = "E:\\";
 	if (argus_size >= 3) {
 		port = arugs[1];
 		root_path = arugs[2];
@@ -26,7 +26,7 @@ int main(std::size_t argus_size, char const* arugs[])
 		else {
 			path = view2str(url.substr(nonstd::string_view("/nginx/").size(), url.size()));
 		}
-		auto fpath = fs::path(root_path + view2str(path));
+		auto fpath = fs::path(root_path + url_decode(view2str(path)));
 		if (fs::is_directory(fpath)) {
 			json root;
 			root["list"]["directors"] = json::array();
@@ -53,8 +53,7 @@ int main(std::size_t argus_size, char const* arugs[])
 			res.write_view("./tmpl/index.html", root,true,http_status::ok);
 		}
 		else {
-			auto path = utf8_to_gbk(url_decode(fpath.string()));
-			res.write_file(path, true); 
+			res.write_file(utf8_to_gbk(fpath.string()), true);
 		}
 	});
 	server.run();
